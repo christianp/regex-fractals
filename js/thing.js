87b73a14357018665271d3f6ef7ac0cceb7206b3
Christian Perfect <christianperfect@gmail.com>
2014-03-27 08:46:36 +0000

    use canvas
    so much faster

--- js/thing.js
+++ js/thing.js
@@ -1,22 +1,39 @@
-$(function() {
-	var width = Math.pow(2,6);
+var grid_width = Math.pow(2,9);
 
-	var grid = document.createElement('table');
-	for(var y=0;y<width;y++) {
-		var row = document.createElement('tr');
-		grid.appendChild(row);
-		for(var x=0;x<width;x++) {
-			var cell = document.createElement('td');
-			row.appendChild(cell);
+function makeGrid() {
+	var canvas = document.getElementById('grid');
+	canvas.setAttribute('width',grid_width);
+	canvas.setAttribute('height',grid_width);
+	var context = canvas.getContext('2d');
+
+	var addresses = [];
+	for(var y=0;y<grid_width;y++) {
+		var row = [];
+		addresses.push(row);
+		for(var x=0;x<grid_width;x++) {
+			row.push('');
 		}
 	}
-	$('#grid').append(grid);
+
+	return {
+		context: context,
+		addresses: addresses
+	};
+}
+
+$(function() {
+
+	var d = makeGrid();
+	var context = d.context;
+	var addresses = d.addresses;
+
+	var data = context.createImageData(grid_width,grid_width);
+	var pixels = data.data;
 
 	function address(x,y,width,path) {
 		if(width==1) {
-			var cell = $('#grid tr').eq(y).find('td').eq(x);
-			cell.data('address',path);
-			cell.attr('address',path);
+			addresses[y][x] = path;
+			pixels[(y*grid_width+x)*4+3] = 255;
 		}
 		else {
 			width /= 2;
@@ -26,7 +43,7 @@ $(function() {
 			address(x+width,y+width,width,path+'4');
 		}
 	}
-	address(0,0,width,'');
+	address(0,0,grid_width,'');
 
 	function makeColour(c) {
 		c = c ? 1/c.length : 1;
@@ -43,20 +60,31 @@ $(function() {
 
 		}
 		catch(e) {
-			$('td').removeClass('on');
 			return;
 		}
-		$('td').each(function() {
-			var match = regex.exec($(this).data('address'));
-			$(this).toggleClass('on',match!==null);
-			if(match) {
-				var col = 'rgb('+makeColour(match[1])+','+makeColour(match[2])+','+makeColour(match[2])+')';
-				$(this).css('background-color',col);
+
+		for(var y=0;y<grid_width;y++) {
+			for(var x=0;x<grid_width;x++) {
+				var address = addresses[y][x];
+				var match = regex.exec(address);
+				var pos = (y*grid_width+x)*4;
+				if(match) {
+					var r = makeColour(match[1]);
+					var g = makeColour(match[2]);
+					var b = makeColour(match[3]);
+					pixels[pos] = r;
+					pixels[pos+1] = g;
+					pixels[pos+2] = b;
+				}
+				else {
+					pixels[pos] = 255;
+					pixels[pos+1] = 255;
+					pixels[pos+2] = 255;
+				}
 			}
-			else {
-				$(this).css('background-color','');
-			}
-		});
+		}
+
+		context.putImageData(data,0,0);
 	}
 	$('#regex').on('change keyup',light);
 	var regex_source = decodeURIComponent(location.search.slice(1)) || '(.*)1(.*)';
